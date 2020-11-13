588ad11ff868004c8b8e8dcb731970c9f7fb5906
Martin Guillon <martin.guillon@akylas.fr>
2020-11-13 14:09:09 +0100

    tes: demo app remove trace

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,7 +1,6 @@
 ﻿import * as application from '@nativescript/core/application';
 import { Trace } from '@nativescript/core';
-import { MapboxTraceCategory } from '@nativescript-community/ui-mapbox';
-console.log('MapboxTraceCategory', MapboxTraceCategory);
-Trace.addCategories(MapboxTraceCategory);
-Trace.enable();
+// import { MapboxTraceCategory } from '@nativescript-community/ui-mapbox';
+// Trace.addCategories(MapboxTraceCategory);
+// Trace.enable();
 application.run({ moduleName: 'main-page' });
