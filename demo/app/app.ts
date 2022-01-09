f77210313d44ae27c306c290d897fe2fd95021a0
Karl Schmaltz <karlschmaltz@gmail.com>
2022-01-09 08:59:29 -0700

    chore: remove old demos

--- demo/app/app.ts
+++
@@ -1,6 +0,0 @@
-﻿import * as application from '@nativescript/core/application';
-import { Trace } from '@nativescript/core';
-// import { MapboxTraceCategory } from '@nativescript-community/ui-mapbox';
-// Trace.addCategories(MapboxTraceCategory);
-// Trace.enable();
-application.run({ moduleName: 'main-page' });
