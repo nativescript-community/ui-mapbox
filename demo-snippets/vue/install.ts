4f266dad7dbf897fe320014b8e538c42eab93fdb
Martin Guillon <dev@akylas.fr>
2025-10-21 10:41:16 +0200

    chore: demo tracing

--- demo-snippets/vue/install.ts
+++ demo-snippets/vue/install.ts
@@ -1,9 +1,13 @@
+import { Trace } from '@nativescript/core';
 import Vue from 'nativescript-vue';
 import Basic from './Basic.vue';
-import { MapboxView } from '@nativescript-community/ui-mapbox';
+import { MapboxTraceCategory, MapboxView } from '@nativescript-community/ui-mapbox';
 
 export function installPlugin() {
     Vue.registerElement('MapboxView', () => MapboxView);
 }
 
 export const demos = [{ name: 'Basic', path: 'Basic', component: Basic }];
+
+Trace.addCategories(MapboxTraceCategory);
+Trace.enable();
