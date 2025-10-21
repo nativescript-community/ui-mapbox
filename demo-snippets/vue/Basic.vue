889afda1adf76badd80015248c85970b81135c34
Martin Guillon <dev@akylas.fr>
2025-10-21 10:42:02 +0200

    chore: demo update

--- demo-snippets/vue/Basic.vue
+++ demo-snippets/vue/Basic.vue
@@ -1,33 +1,55 @@
 <template>
     <Page>
-        <GridLayout>
+        <GridLayout rows="*,auto">
             <MapboxView
-                accessToken="pk.eyJ1IjoiYWt5bGFzIiwiYSI6IkVJVFl2OXMifQ.TGtrEmByO3-99hA0EI44Ew"
+                accessToken="pk.eyJ1IjoiYWt5bGFzIiwiYSI6ImNtaDBhNGp4ajBhbjQ1dnM4dzIwYXh1NjcifQ.iQt8KEQ2YfulTZuA1BQp2w"
                 mapStyle="traffic_day"
                 latitude="50.467735"
                 longitude="13.427718"
                 hideCompass="true"
-                zoomLevel="18"
+                zoomLevel="13"
                 showUserLocation="false"
                 disableZoom="false"
                 disableRotation="false"
                 disableScroll="false"
                 disableTilt="false"
+                rowSpan="2"
                 @mapReady="onMapReady($event)"
-            >
-            </MapboxView>
+            />
+            <StackLayout orientation="horizontal" row="1">
+                <button text="marker" @tap="addMarker($event)" />
+            </StackLayout>
         </GridLayout>
     </Page>
 </template>
 
 <script lang="ts">
+import { MapboxView } from '@nativescript-community/ui-mapbox';
 export default {
     data() {
-        return {};
+        return {
+            map: null
+        };
     },
     methods: {
-        onMapReady(args): void {
-            console.log('map is ready');
+        onMapReady({ map }: { map: MapboxView }): void {
+            this.map = map;
+            console.log('map is ready', map);
+        },
+        addMarker(event) {
+            console.log('addMarker', this.map);
+            this.map.addMarkers([
+                {
+                    lat: 50.467735,
+                    lng: 13.427718,
+                    title: 'One-line title here',
+                    subtitle: 'Really really nice location',
+                    selected: true, // makes the callout show immediately when the marker is added (note: only 1 marker can be selected at a time)
+                    onCalloutTap() {
+                        console.log("'Nice location' marker callout tapped");
+                    }
+                }
+            ]);
         }
     }
 };
