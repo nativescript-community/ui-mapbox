aab77b78f40ee5ca0dec5637390d8a38ec7f7f66
Martin Guillon <dev@akylas.fr>
2026-06-26 09:56:50 +0200

    fix: iOS now uses swift package allowing user to override version

--- packages/ui-mapbox/nativescript.config.ts
+++ packages/ui-mapbox/nativescript.config.ts
@@ -1,7 +1,12 @@
 export default {
   ios: {
     SPMPackages: [
-      
+      {
+          name: 'MapboxMaps',
+          libs: ['MapboxMaps'],
+          repositoryURL: 'https://github.com/mapbox/mapbox-maps-ios.git',
+          version: '11.25.0'
+      }
     ],
   },
 }
